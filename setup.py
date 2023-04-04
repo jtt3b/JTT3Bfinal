1e69826b5065178c4ee686c6f7000a43943327fc
jtt3b <125292992+jtt3b@users.noreply.github.com>
2023-04-04 06:09:04 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='final project capstone',
+    author='Joe Thomas',
+    license='MIT',
+)
